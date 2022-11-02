a9330b7c77599d6a147c3dcf7d56e95a96853dcf
Claudiu Cornea <claudiu.simion.cornea@gmail.com>
2022-11-02 17:34:12 +0100

    Update gatsby-config.js and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}"
+     "./src/pages/*.{js,jsx,ts,tsx}",
+     "./src/components/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
