a942392bf1e261cfbcb37c6526d15c237e40871a
Feruz <59234162+Feruzteame@users.noreply.github.com>
2022-11-02 16:31:16 +0100

    tailwind problem

---
+++ src/components/Hero.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import HeroImg from '../img/new_design/Mask group.svg'
+import HeroBg from '../img/new_design/Hero 1.svg'
+
+const backgroundImage = {
+  backgroundImage: `url('${HeroBg}')`,
+  backgroundSize: 'cover',
+  position: 'center'
+};
+
+export default function Hero() {
+  return (
+    <div
+    style={backgroundImage}
+    className='
+    container
+    flex flex-col lg:flex-row 
+    px-20
+    h-screen
+    text-center'>
+      <div className='hidden md:flex self-center w-full'>
+        <img
+        src={HeroImg}
+        alt='PC'
+        style={{ width:'500px', height: '400px' }}/>
+      </div>
+      <div className='
+      flex flex-col self-center 
+      h-auto w-full
+      gap-8
+      text-white'>
+        <h1 className='text-5xl'>
+          We are making VC investing even simpler than public stock investing
+        </h1>
+        <p className='text-2xl'>
+          We do more than enabling investors to allocate to top decile VCs across vintages, 
+          regions and sectors from as low as 10k GBP. We also eliminate the entire admin GPs
+          have to go through to on-board, serve and report to LPs.
+        </p>
+        <div className=''>
+          <button className='px-4 py-2 
+          bg-blue-600 text-white
+          rounded'>
+            Info Request
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      }
+    },
   },
   plugins: [],
 }
