022f8c2442a16d6482d0b6ff5ca108588c4daa74
Feruz <59234162+Feruzteame@users.noreply.github.com>
2022-11-27 22:06:28 +0100

    update css

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,6 @@ module.exports = {
         'darkHeroBlue': '#3273DC',
         'lightHeroYellow': '#E7B224',
         'box-shadow': '0px 0px 32px rgba(74, 74, 74, 0.75)',
-        'backdrop': 'blur(7.5px)',
       },
       height: {
         'hrHeight': '3px'
