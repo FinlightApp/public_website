2271ca3b18d9c47d61ae374c42e4629b11607530
matt-harding-2021 <09maharding@gmail.com>
2022-12-06 11:50:31 +0000

    update

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ export default function Button(props) {
   return (
     <React.Fragment>
       <button className={`
-        ${ props.class || 'bg-primary text-black' }
+        ${ props.class }
         px-4 py-2
         rounded
         text-xl font-medium font-paragraph
