71f653cee50d3a7689cf2f8bff6835649189f983
matt-harding-2021 <09maharding@gmail.com>
2022-12-07 16:56:19 +0000

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
-import * as React from "react";
-import { Link } from "gatsby";
+import * as React from 'react';
+import { Link } from 'gatsby';
 
-import finlight_logo from "../img/finlight_logo_f6.svg";
-import twitter_logo from "../img/twitter_logo.svg";
-import linkedIn_logo from "../img/linkedin_logo.svg";
+import finlight_logo from '../img/finlight_logo_f6.svg';
+import twitter_logo from '../img/twitter_logo.svg';
+import linkedIn_logo from '../img/linkedin_logo.svg';
 
 const Footer = () => {
   return (
@@ -15,14 +15,14 @@ const Footer = () => {
       bg-black
     '>
       <img className='h-12'
-      src={finlight_logo} alt="logo" />
+      src={finlight_logo} alt='logo' />
       <div class='flex gap-8 md:gap-16'>
-        <Link className="text-white"
-        to="/">
+        <Link className='text-white'
+        to='/'>
           Contact
         </Link>
-        <Link className="text-white"
-        to="/">
+        <Link className='text-white'
+        to='/'>
           Privacy
         </Link>
       </div>
@@ -30,20 +30,20 @@ const Footer = () => {
         flex
         gap-8
       '>
-        <a href="https://twitter.com/FinlightNews" target="_blank"
-        referrerpolicy="no-referrer"
-        rel="noopener noreferrer">
-          <img src={twitter_logo} alt="Twitter"
+        <a href='https://twitter.com/FinlightNews' target='_blank'
+        referrerpolicy='no-referrer'
+        rel='noopener noreferrer'>
+          <img src={twitter_logo} alt='Twitter'
           className='h-4 w-4' />
         </a>
-        <a href="https://www.linkedin.com/company/finlight" target="_blank"
-        referrerpolicy="no-referrer"
-        rel="noopener noreferrer">
-          <img src={linkedIn_logo} alt="LinkedIn"
+        <a href='https://www.linkedin.com/company/finlight' target='_blank'
+        referrerpolicy='no-referrer'
+        rel='noopener noreferrer'>
+          <img src={linkedIn_logo} alt='LinkedIn'
           className='h-4 w-4' />
         </a>
       </div>
-      <p className="font-light text-xs text-white">
+      <p className='font-light text-xs text-white'>
         Ⓒ MONEY-PEASY LTD - FINLIGHT.COMTM 2022
       </p>
     </footer>
