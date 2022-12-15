09555d8e9c5e24ad29ecd79bd30c50c6d4b4782b
Taguhi <t.karakashyan@finlight.com>
2022-12-15 11:41:40 +0100

    article page update

--- src/components/Article.js
+++ src/components/Article.js
@@ -7,87 +7,81 @@ import linkedIn from '../img/linkedin_logo.svg';
 import Panel from './Panel';
 
 export const HTMLContent = ({ content, className }) => (
-<>
-        <div>
-            <p>header</p>
+  <>
+    <div>
+      <p>header</p>
+    </div>
+    <div className='
+      flex flex-row
+      justify-center items-start
+      p-12
+      gap-12
+      h-auto
+      w-full
+    '>
+      <div className='
+        flex flex-col
+        gap-6
+        w-[60%]
+      '>
+        <div className='
+          h-[500px]
+          w-full
+          overflow-y-scroll
+        '>
+          <div className={className} dangerouslySetInnerHTML={{ __html: content }} />  
         </div>
         <div className='
-            flex flex-row
-            justify-center items-start
-            p-12
-            gap-12
-            h-auto
-            w-full
-        '>
-            <div className='
-                flex flex-col
-                gap-6
-                w-[60%]
-            '>
-            <div className='
-                h-[500px]
-                w-full
-                overflow-y-scroll
-            '>
-                <div className={className} dangerouslySetInnerHTML={{ __html: content }} />  
-            </div>
-            <div className='
-                    flex flex-row
-                    justify-end
-                    gap-12
-                '>
-                    <a href='https://twitter.com/FinlightNews' target='_blank'
-        referrerPolicy='no-referrer'
-        rel='noopener noreferrer'>
-            <img src={twitter} alt='Twitter'
-            className='
-            h-4 w-4
-            bg-blue-500
-            '/>
-        </a>
-        <a href='https://www.linkedin.com/company/finlight' target='_blank'
-        referrerPolicy='no-referrer'
-        rel='noopener noreferrer'>
-            <img src={linkedIn} alt='LinkedIn'
-            className='
+          flex flex-row
+          justify-end
+          gap-12
+          '>
+            <a href='https://twitter.com/FinlightNews' target='_blank'
+              referrerPolicy='no-referrer'
+              rel='noopener noreferrer'>
+              <img src={twitter} alt='Twitter'
+              className='
                 h-4 w-4
-            ' />
-        </a>
-        <a href='https://www.linkedin.com/company/finlight' target='_blank'
-        referrerPolicy='no-referrer'
-        rel='noopener noreferrer'>
-            <img src={linkedIn} alt='LinkedIn'
-            className='
-                h-4 w-4
-            ' />
-        </a>
-                    
-                </div>
-                </div>
-            <div className='
-                flex flex-col
-                justify-center
-                gap-6
-                h-auto
-                w-[40%]
-            '>
-                
-            <img className='
-                w-full h-auto
                 bg-blue-500
-            '
-            src= {image}
-            alt= 'image'
-            draggable='false' />
-            <p className='
-                px-6
-                text-center
-                italic
-            '>
-            Every year, over 12 million people in the UK go through the ordeal of tax preparation.
-            </p>
-            </div>
+              '/>
+            </a>
+            <a href='https://www.linkedin.com/company/finlight' target='_blank'
+              referrerPolicy='no-referrer'
+              rel='noopener noreferrer'>
+              <img src={linkedIn} alt='LinkedIn'
+              className='h-4 w-4' />
+            </a>
+            <a href='https://www.linkedin.com/company/finlight' target='_blank'
+              referrerPolicy='no-referrer'
+              rel='noopener noreferrer'>
+              <img src={linkedIn} alt='LinkedIn'
+              className='h-4 w-4' />
+            </a>
         </div>
+      </div>
+      <div className='
+        flex flex-col
+        justify-center
+        gap-6
+        h-auto
+        w-[40%]
+      '>
+        <img className='
+          w-full h-auto
+          bg-blue-500
+        '
+        src= {image}
+        alt= 'image'
+        draggable='false' />
+        <p className='
+          px-6
+          text-center
+          italic
+        '>
+          Every year, over 12 million people in the UK go through the ordeal of tax preparation.
+        </p>
+      </div>
+    </div>
         {/* <div className='grid grid-cols-10'>
             <div className='
                 col-span-10
