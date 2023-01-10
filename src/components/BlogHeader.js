de9c8bb3455aeae869a14a61a96fe017d32066db
Feruz <59234162+Feruzteame@users.noreply.github.com>
2023-01-10 18:03:09 +0100

    update css

--- src/components/BlogHeader.js
+++ src/components/BlogHeader.js
@@ -16,60 +16,54 @@ export default function BlogHeader(props){
       { ( image || title || author || date ) && (
         <div className='
           relative
-          flex lg:flex-row
-          w-full
-          p-6 md:p-12
-          z-0
+          container
+          overflow-hidden
+          flex flex-col lg:flex-row
+          m-6 md:m-12
+          w-fit h-fit
+          z-0 rounded-2xl
         '>
-          <div className='
-            container
-            overflow-hidden
-            flex flex-col lg:flex-row justify-center
-            w-full h-fit
-            z-10 rounded-2xl
-          '>
+          <div className='flex w-full lg:w-[50%] lg:h-96'>
             { image?.src?.url ? (
-              <img className='w-full lg:w-[50%] lg:h-96'
+              <img className='w-full h-full'
               src={ getImage(image.src) || image.src }
               alt={ image.alt } />
             ) : (
-              <GatsbyImage className='w-full lg:w-[50%] lg:h-96'
+              <GatsbyImage className='w-full h-full'
               formats={ ['auto', 'webp'] }
               image={ getImage(image.src) || image.src }
               alt={ image.alt } />
             ) }
-            <div className='
-              flex  flex-col justify-center
-              w-full lg:max-w-[50%] lg:h-96
-              p-6 md:p-12
-              gap-3 lg:gap-6
-              bg-[#F6F6F6]
-            '>
-              { ( title &&
-                <h1 className='
-                  text-left
-                  text-4xl font-bold leading-[48px]
-                '>
-                  { title }
-                </h1>
-              ) }
-              <div className='flex flex-col justify-center items-start'>
-                { ( author &&
-                  <p className='
-                    lg:hidden
-                    text-sm font-light leading-6
-                  '>
-                    { author }
-                  </p>
-                ) }
-                { ( date &&
-                  <p className='text-xs font-extralight leading-6'>
-                    { date }
-                  </p>
-                ) }
-            </div>
-           </div>
           </div>
+          <div className='
+            flex flex-col justify-center
+            w-full lg:max-w-[50%] lg:h-96
+            p-6 md:p-12
+            gap-3 lg:gap-6
+            bg-[#F6F6F6]
+          '>
+            { ( title &&
+              <h1 className='
+                text-left
+                text-4xl font-bold leading-[48px]
+              '>
+                { title }
+              </h1>
+            ) }
+            { ( author &&
+              <p className='
+                lg:hidden
+                text-sm font-light leading-6
+              '>
+                { author }
+              </p>
+            ) }
+            { ( date &&
+              <p className='text-xs font-extralight leading-6'>
+                { date }
+              </p>
+            ) }
+         </div>
         </div>
       ) }
     </>
