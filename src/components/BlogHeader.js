b3c7a7d4e031e781a3dc776b7114a0a633583326
Feruz <59234162+Feruzteame@users.noreply.github.com>
2023-01-10 17:09:01 +0100

    update article Hero

--- src/components/BlogHeader.js
+++ src/components/BlogHeader.js
@@ -1,9 +1,11 @@
 import * as React from "react";
 import PropTypes from 'prop-types';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 export default function BlogHeader(props){
 
   const {
+    image,
     title,
     author,
     date
@@ -11,48 +13,59 @@ export default function BlogHeader(props){
 
   return(
     <>
-      { ( title || author || date ) && (
+      { ( image || title || author || date ) && (
         <div className='
           relative
-          flex
-          w-full h-fit
+          flex lg:flex-row
+          w-full
+          p-6 md:p-12
           z-0
         '>
-          <div className='absolute inset-0 bg-[#F6F6F6] bg-cover z-0' />
           <div className='
             container
-            flex-1
-            flex flex-col justify-center
-            py-12
-            gap-10 lg:gap-12
-            z-10
+            overflow-hidden
+            flex flex-col lg:flex-row justify-center
+            w-full h-fit
+            z-10 rounded-2xl
           '>
-            { ( title &&
-              <h1 className='
-                px-6 md:px-12
-                lg:max-w-[60%]
-                text-left
-                text-4xl font-bold leading-[48px]
-              '>
-                { title }
-              </h1>
+            { image?.src?.url ? (
+              <img className='w-full lg:w-[50%] lg:h-96'
+              src={ getImage(image.src) || image.src }
+              alt={ image.alt } />
+            ) : (
+              <GatsbyImage className='w-full lg:w-[50%] lg:h-96'
+              formats={ ['auto', 'webp'] }
+              image={ getImage(image.src) || image.src }
+              alt={ image.alt } />
             ) }
             <div className='
-              px-6 md:px-12
-              flex flex-col
-              justify-center items-end
+              flex  flex-col justify-center
+              w-full lg:max-w-[50%] lg:h-96
+              p-6 md:p-12
+              gap-3 lg:gap-6
+              bg-[#F6F6F6] 
             '>
-              { ( author &&
-                <p className='text-sm font-light leading-6'>
-                  { author }
-                </p>
+              { ( title &&
+                <h1 className='
+                  text-left
+                  text-4xl font-bold leading-[48px]
+                '>
+                  { title }
+                </h1>
               ) }
-              { ( date &&
-                <p className='text-xs font-extralight leading-6'>
-                  { date }
-                </p>
-              ) }
+              <div className='flex flex-col justify-center items-start'>
+                { ( author &&
+                  <p className='lg:hidden text-sm font-light leading-6'>
+                    { author }
+                  </p>
+                ) }
+                { ( date &&
+                  <p className='text-xs font-extralight leading-6'>
+                    { date }
+                  </p>
+                ) }
             </div>
+           </div>
           </div>
         </div>
       ) }
