90dab7041ffdb68c78dbd85659a4a2f7d40d7076
Claudiu Cornea <claudiu.simion.cornea@gmail.com>
2023-05-04 17:13:04 +0200

    added G tag

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import { Script } from "gatsby";
 
 export default function Head(props) {
   const {
@@ -19,6 +20,18 @@ export default function Head(props) {
         <meta property='og:type' content='business.business' />
         <meta property='og:title' content={ seo?.title || 'Finlight' } />
         <meta name='theme-color' content='#fff' />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-8KF6HM5V9M`}
+          strategy="off-main-thread"
+        />
+        <Script id="gtag-config" strategy="off-main-thread" forward={[`gtag`]}>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)};
+            gtag('js', new Date());
+            gtag('config', 'G-8KF6HM5V9M', { page_path: location ? location.pathname + location.search + location.hash : undefined })
+          `}
+        </Script>
     </>
   );
 }
